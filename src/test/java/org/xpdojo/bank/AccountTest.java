a53758c29fe02a18f112ed219ef7bef0c53ea2b9
Neal Harvey <neal@nhsk.org>
2020-07-16 15:08:21 +0100

    New account is zeroed

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,12 +3,18 @@ package org.xpdojo.bank;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void newAccountIsZero() {
+        Account testAcc = new Account();
+        assertThat(testAcc.balance).isEqualTo(0);
+    }
+
+    @Test
+    @Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
     }
